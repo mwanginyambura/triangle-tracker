5e7c36ecaea41252f5a76f03dfaa5562a609df5e
mwanginyambura <mwanginyambura@gmail.com>
2019-02-15 07:54:45 -0500

    Add input form

---
+++ script.js
@@ -0,0 +1,11 @@
+function sides(){
+var sidex= .getElementById('x').value();
+var sidey= .getElementById('y').value();
+var sidez= .getElementById('z').value();
+
+var x= parseInt(sidex);
+var y= parseInt(sidey);
+var z=parseInt(sidez);
+if x===y && y===z && x===z;
+alert("equiateral triangle");
+}
