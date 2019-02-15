ca8500f9d6997a36e075c931743eb425d93a1052
mwanginyambura <mwanginyambura@gmail.com>
2019-02-15 10:38:25 -0500

    Add a closing curly brackets

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 function sides(){
-var sidex= Document.getElementById('x').value();
-var sidey= Document.getElementById('y').value();
-var sidez= Document.getElementById('z').value();
+var sidex= document.getElementById('x').value;
+var sidey= document.getElementById('y').value;
+var sidez= document.getElementById('z').value;
 
 var x= parseInt(sidex);
 var y= parseInt(sidey);
 var z= parseInt(sidez);
-if (x===y && y===z && x===z){
-alert("equiateral triangle");
-}else if (x===y || y===z || x===z){
-alert("isosceles triangle");
-}else if (x!=y || y!=z || x!=z && x+y>z){
-alert("scalene triangle");
+if(x+y>z && y+z>x && z+x>y){
+  if (x===y && y===z && x===z){
+  alert("equilateral triangle");
+  }else if (x===y || y===z || x===z){
+  alert("isoscele triangle");
+  }else if (x!==y || y!==z || x!==z){
+  alert("scalene triangle");
+  }
+}else{
+  alert("Not a triangle")
+}
 }
