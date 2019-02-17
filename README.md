f561830e18d30f786523063e5aa9b8245252772c
mwanginyambura <mwanginyambura@gmail.com>
2019-02-17 09:02:58 -0500

    remove returns in the javascript

---
+++ README.md
@@ -0,0 +1,17 @@
+\##TRIANGLE-TRACKER
+By Mwangi Nyambura.
+\###Description
+This is a site where individuals can be able to differentiate types of triangles using their dimensions.
+\###Technologies used
+I used:HTML
+       CSS
+       JAVASCRIPT
+\###SETUP INSTRUCTIONS
+A user should enter dimensions of a triangle in the input fields provided then click on the submit button to find out the type of triangle you've created.
+\###Contact
+mwanginyambura19@gmmail.com
+
+###
+
+MIT
+copyright (c) 2019
--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-function sides(){
-var sidex= document.getElementById('x').value;
-var sidey= document.getElementById('y').value;
-var sidez= document.getElementById('z').value;
+function sides() {
+  var sidex = parseInt(document.getElementById('x').value);
+  var sidey = parseInt(document.getElementById('y').value);
+  var sidez = parseInt(document.getElementById('z').value);
 
-var x= parseInt(sidex);
-var y= parseInt(sidey);
-var z= parseInt(sidez);
-if(x+y>z && y+z>x && z+x>y){
-  if (x===y && y===z && x===z){
-  alert("equilateral triangle");
-  }else if (x===y || y===z || x===z){
-  alert("isoscele triangle");
-  }else if (x!==y || y!==z || x!==z){
-  alert("scalene triangle");
+  if (sidex + sidey > sidez && sidey + sidez > sidex && sidez + sidex > sidey) {
+    if (sidex == sidey && sidey == sidez && sidex == sidez) {
+      alert("equilateral triangle");
+      // return "equilateral triangle";
+    }
+    // else if (x===y || y===z || x===z){
+    //   alert("isoscele triangle");
+    //   return "isoscele triangle";
+    //   }
+    else if (sidex != sidey && sidey != sidez && sidex != sidez) {
+      alert("scalene triangle");
+      // return "scalen triangle";
+    } else {
+      alert("this is an isosceles");
+      // return "this is an isosceles";
+    }
+  } else {
+    alert("Not a triangle");
+    // return "Not a triangle"
   }
-}else{
-  alert("Not a triangle")
-}
 }
